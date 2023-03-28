e9718c704ebfc570c6dc1de72f1d96895544f8f7
xrLil Batya <75887982+xrLil-Batya@users.noreply.github.com>
2023-03-28 17:06:22 +0400

    Лоадскрин в xml из OpenXRay

--- src/xrGame/GamePersistent.cpp
+++ src/xrGame/GamePersistent.cpp
@@ -38,6 +38,10 @@
 #	include "ai_debug.h"
 #endif // _EDITOR
 
+#include "string_table.h"
+#include "../xrEngine/x_ray.h"
+#include "ui/UILoadingScreen.h"
+
 //#ifdef DEBUG_MEMORY_MANAGER
 //	static	void *	ode_alloc	(size_t size)								{ return Memory.mem_alloc(size,"ODE");			}
 //	static	void *	ode_realloc	(void *ptr, size_t oldsize, size_t newsize)	{ return Memory.mem_realloc(ptr,newsize,"ODE");	}
@@ -107,6 +111,12 @@ CGamePersistent::~CGamePersistent(void)
 	Device.seqFrame.Remove		(this);
 	Engine.Event.Handler_Detach	(eDemoStart,this);
 	Engine.Event.Handler_Detach	(eQuickLoad,this);
+}
+
+void CGamePersistent::PreStart(LPCSTR op)
+{
+	pApp->SetLoadingScreen(new UILoadingScreen());
+	__super::PreStart(op);
 }
 
 void CGamePersistent::RegisterModel(IRenderVisual* V)
@@ -804,8 +814,7 @@ void CGamePersistent::OnRenderPPUI_PP()
 {
 	MainMenu()->OnRenderPPUI_PP();
 }
-#include "string_table.h"
-#include "../xrEngine/x_ray.h"
+
 void CGamePersistent::LoadTitle(bool change_tip, shared_str map_name)
 {
 	pApp->LoadStage();
